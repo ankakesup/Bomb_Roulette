95f43ff607e8ecb99ff8fd0b55de6b40b9da9999
Koki Ohno <241205032@ccmailg.meijo-u.ac.jp>
2025-03-24 19:05:31 +0900

    repair_TurnText
    ・TurnTextの文字が更新されるようにした。

--- Assets/Scripts/Core/TurnManager.cs
+++ Assets/Scripts/Core/TurnManager.cs
@@ -2,17 +2,33 @@
 using UnityEngine;
 using System.Collections.Generic;
 using Bomb_Roulette.Models;
+using Bomb_Roulette.UI;
 
 namespace Bomb_Roulette.Core
 {
     public class TurnManager : MonoBehaviour
     {
+        public static TurnManager Instance;
         private int currentTurn = 0; // 現在のターン(Nターン目に対して，N-1の値を取る)
         private int numPlayers = 0; // プレイヤーの人数の管理
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
 
         public void SetNumPlayers(int numPlayersTemp) // TitleScreenUI からプレイヤーの人数を取得する関数
         {
             numPlayers = numPlayersTemp;
+
         }
 
         public int GetNumPlayers() // numPlayers の値を返す関数
@@ -22,11 +38,12 @@ namespace Bomb_Roulette.Core
 
         public void NextTurn() // 次のターンにする関数
         {
-            currentTurn++;
+            currentTurn = currentTurn + 1;
             if (currentTurn == numPlayers) // 全員のターンが終わったらラウンドを更新する
             {
                 // 次のaまでのコードで RoundManager の NextRound関数 の呼び出しを行う
-                RoundManager roundManager = new RoundManager();
+                GameObject gameObjectRound = new GameObject("Bomb_Roulette.Core.RoundManager");
+                Bomb_Roulette.Core.RoundManager roundManager = gameObjectRound.AddComponent<Bomb_Roulette.Core.RoundManager>();
                 roundManager.NextRound();
                 // a
                 currentTurn = 0; // ラウンドのはじめにcurrentTurnを0にリセットする
@@ -34,15 +51,19 @@ namespace Bomb_Roulette.Core
             UpdateTurnUI(); // UIの更新
         }
 
-        void UpdateTurnUI() // ターンの変更時にUIを更新する関数
+        void UpdateTurnUI()
         {
-            Debug.Log("Turn: Player" + (currentTurn + 1) ); // デバッグログの出力
+            Debug.Log("Turn: Player" + (currentTurn + 1));
 
-            // 次のbまでのコードで GameUI の UpdateTurnDisplay関数 の呼び出しを行う
-            GameObject gameObject = new GameObject("Bomb_Roulette.UI.GameUI");
-            Bomb_Roulette.UI.GameUI gameUI = gameObject.AddComponent<Bomb_Roulette.UI.GameUI>();
-            gameUI.UpdateTurnDisplay(currentTurn + 1);
-            // b
+            if (GameUI.Instance != null)
+            {
+                GameUI.Instance.UpdateTurnDisplay(currentTurn + 1);
+            }
+            else
+            {
+                Debug.LogError("GameUI のインスタンスが見つかりません。シーンに配置されているか確認してください。");
+            }
         }
+
     }
 }
