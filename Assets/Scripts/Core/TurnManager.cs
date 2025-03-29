8aba02b7684edd0a0506bcc7cd596a32d660c878
Kawamoto Raiou <241205063@ccmailg.meijo-u.ac.jp>
2025-03-29 14:30:29 +0900

    導火線の選択によるターン進行
    導火線を選択するとターンが進行するようにした

--- Assets/Scripts/Core/TurnManager.cs
+++ Assets/Scripts/Core/TurnManager.cs
@@ -81,14 +81,6 @@ namespace Bomb_Roulette.Core
 
         public void NextTurn() // 次のターンにする関数
         {
-            //-------後に導火線を決定したタイミングで実行するように変更する--------------------
-            // 現在のプレイヤーが爆発するかチェック
-            if (CheckForExplosion())
-            {
-                return; // 爆発したらゲーム終了するので、次のターンへ進まない
-            }
-            //--------------------------------------------------------------------------
-
             currentTurn = currentTurn + 1;
             if (currentTurn == numPlayers) // 全員のターンが終わったらラウンドを更新する
             {
